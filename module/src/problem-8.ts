f24346928740648c52f6e087c7c1303ae9868306
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-11-07 02:16:20 +0600

    solve problem 8

---
+++ module/src/problem-8.ts
@@ -0,0 +1,25 @@
+{
+  type Person = {
+    name: string;
+    age: number;
+    email: string;
+  };
+
+  const validateKeys = <T extends Person>(obj: T,keys: (keyof T)[]): boolean => {
+    for (const key of keys) {
+      if (!(key in obj)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const person: Person = {
+    name: "Alice",
+    age: 25,
+    email: "alice@example.com",
+  };
+
+  const result = validateKeys(person, ["name", "age"])
+//   console.log(result);
+}
