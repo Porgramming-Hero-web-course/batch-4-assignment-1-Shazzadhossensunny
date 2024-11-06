6607e8a8a8531eafbb7c7e059ca9be3dc6b41264
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-11-06 20:28:57 +0600

    solve problem 3

---
+++ module/src/problem-3.ts
@@ -0,0 +1,22 @@
+{
+
+
+    const countWordOccurrences = (sentence:string, word:string):number =>{
+        const punctuation = /[\.,?!]/g;
+        const newSentence = sentence.replace(punctuation, '').split(" ");
+        let count = 0;
+        for (let i = 0; i < newSentence.length; i++) {
+           if(newSentence[i].toLowerCase() === word.toLowerCase()){
+             count = count + 1;
+           }
+        }
+
+      return count;
+
+
+    }
+
+    const result = countWordOccurrences("TypeScript is great. I love TypeScript!", "Great")
+    console.log(result)
+
+}
